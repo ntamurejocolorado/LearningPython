d80fc871fa16fb30c9b794d51703b9fad7d305e0
Noelia Tamurejo Colorado <noelia.tc89@gmail.com>
2017-09-10 15:45:35 +0200

    Add helloword.py with some examples lesson 1

---
+++ helloworld.py
@@ -0,0 +1,25 @@
+
+
+# Hello World!
+
+print("This line will be printed.")
+print("Esto" "      " "son" "     " "espacios")
+print ("Sin parentesis")
+
+# Indentacion
+# Python uses indentation for blocks. The standard indentation requires standard Python code to use four spaces.
+
+x = 1
+if x == 1:
+    # indented four spaces
+    print("x is 1.")
+
+
+# Exercise 1
+# Use print to show anything.
+
+print("Hello World!")
+
+print("Goodbye, World!")
+
+print ("The result is:", 2)
